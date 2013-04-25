24bc6135fb861f322476c8432a475371bf3b413b
baptistelemoine <baptiste.lemoine@gmail.com>
2013-04-25 17:17:13 +0200

    checkbox, radio ok

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,10 +1,19 @@
 (function($){
 
-  //ComboBox
-  $('[data-toggle=dropdown]').on('click', function (e){ 
+  //COMBO BOX SETTINGS
+  //on btn expand click
+  $('[data-toggle=dropdown]').on('click', function (e){
     var $current = $(e.currentTarget);
     $current.toggleClass('button-enabled');
-    $current.parent().find('[data-role=combo-submenu]').toggle();
+    $current.parent().css('z-index', 1000).find('[data-role=combo-submenu]').toggle();
   });
+  //stop hover btn effect
+  $('[data-toggle=dropdown]').hover(
+	function (e){
+		if($(this).hasClass('button-enabled')) $(this).css('border', 0);
+	}
+  );
+
+  
 
 }(jQuery));
