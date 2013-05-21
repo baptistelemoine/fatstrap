407ae8ee1e78b75a6316665348bacb3c90bbd364
baptistelemoine <baptiste.lemoine@gmail.com>
2013-05-21 11:34:36 +0200

    layout combo menu with accordion

--- public/js/layout.js
+++ public/js/layout.js
@@ -6,23 +6,16 @@
 
   $(window).on('resize', function(e){
       console.log($(this).width());
-  })
-  
-
-  //COMBO BOX SETTINGS
-  //on btn expand click
-  $('[data-toggle=dropdown]').on('click', function (e){
-    var $current = $(e.currentTarget);
-    $current.toggleClass('button-enabled');
-    $current.parent().css('z-index', 1000).find('[data-role=combo-submenu]').toggle();
   });
-  //stop hover btn effect
-  $('[data-toggle=dropdown]').hover(
-	function (e){
-		//if($(this).hasClass('button-enabled')) $(this).css('border', 0);
-	}
-  );
-
   
+  //Only one at a time is possible
+  $('#handler-filter').on('click', function(e){
+    if($('#handler-combo:checked'))
+      $('#handler-combo:checked').trigger('click');
+  });
+  $('#handler-combo').on('click', function(e){
+    if($('#handler-filter:checked'))
+      $('#handler-filter:checked').trigger('click');
+  });
 
 }(jQuery));
